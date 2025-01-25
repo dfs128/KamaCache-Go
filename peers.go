9ef5153f49b194efdecc12b64b3585aac748b2e7
juguagua <juguaguaji@gmail.com>
2025-01-25 15:21:24 +0800

    feat: peer init

--- peers.go
+++ peers.go
@@ -1,1 +1,24 @@
 package lc_cache
+
+import (
+	"github.com/juguagua/lc-cache/consistenthash"
+	"sync"
+)
+
+// PeerPicker must be implemented to locate the peer that owns a specific key
+type PeerPicker interface {
+	PickPeer(key string) (peer PeerGetter, ok bool, isSelf bool)
+}
+
+// PeerGetter must be implemented by a peer
+type PeerGetter interface {
+	Get(group string, key string) ([]byte, error)
+	Delete(group string, key string) (bool, error)
+}
+
+type ClientPicker struct {
+	self        string // self ip
+	serviceName string
+	mu          sync.RWMutex        // guards
+	consHash    *consistenthash.Map // stores the list of peers, selected by specific key
+}
